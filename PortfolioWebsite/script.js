3876a0a4c6963dcecc7c63717ab5eece8733c321
rennaMAhcuS <rennaMAhcuS@users.noreply.github.com>
2024-05-03 21:11:05 +0530

    Updated some CSS

--- PortfolioWebsite/script.js
+++ PortfolioWebsite/script.js
@@ -1,6 +1,7 @@
 function showTime() {
     const clock = document.getElementById('clock');
     const now = new Date();
+    // clock.innerHTML = now.toLocaleTimeString();
     clock.innerHTML = "Time (in 24 Hr): " + now.toLocaleTimeString();
     setTimeout(showTime, 1000);
 }
